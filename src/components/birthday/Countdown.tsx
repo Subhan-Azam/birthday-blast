9e1b3dc447221bdd50453f6833934307336f447e
subhan <shiekhsubhan2005@gmail.com>
2026-03-04 00:56:36 +0500

    add proper countdown

--- src/components/birthday/Countdown.tsx
+++ src/components/birthday/Countdown.tsx
@@ -14,13 +14,9 @@ const Countdown = ({ targetDate, onComplete }: CountdownProps) => {
     if (difference <= 0) return { days: 0, hours: 0, minutes: 0, seconds: 0 };
     return {
       days: Math.floor(difference / (1000 * 60 * 60 * 24)),
-    //   hours: Math.floor((difference / (1000 * 60 * 60)) % 24),
-    //   minutes: Math.floor((difference / 1000 / 60) % 60),
-    //   seconds: Math.floor((difference / 1000) % 60),
-
-      hours: 0,
-      minutes: 0,
-      seconds: 0,
+      hours: Math.floor((difference / (1000 * 60 * 60)) % 24),
+      minutes: Math.floor((difference / 1000 / 60) % 60),
+      seconds: Math.floor((difference / 1000) % 60),
     };
   }
 
